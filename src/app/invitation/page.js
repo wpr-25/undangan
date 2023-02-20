bed5fbc2ea7330465ea76ab7cc7e81ef6b5f1d82
wpr-25 <willy.ok1025@gmail.com>
2023-02-20 17:13:36 +0700

    first commit

---
+++ src/app/invitation/page.js
@@ -0,0 +1,162 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+export default function invitation() {
+    return (
+        <div>
+            <div className="max-w-7xl mx-auto">
+            <div class="bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:text-center">
+      <img src="https://keishaglow.com/wp-content/uploads/2021/03/logo-keisha-gold-gradasi-gelap.png" class="text-lg font-semibold leading-8 tracking-tight text-indigo-600 "/>
+      <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">INVITATION</p>
+      <p class="mt-6 text-lg leading-8 text-gray-600">GRAND OPENING.</p>
+    </div>
+    <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+      <dl class="grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+        <div class="relative pl-16">
+          <dt class="text-base font-semibold leading-7 text-gray-900">
+            <div class="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+              
+            </div>
+            Nama Acara : 
+          </dt>
+          <dd class="mt-2 text-base leading-7 text-gray-600">Grand Opening Keisha Glow Jakarta.</dd>
+        </div>
+
+        <div class="relative pl-16">
+          <dt class="text-base font-semibold leading-7 text-gray-900">
+            <div class="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+         
+            </div>
+            Tanggal
+          </dt>
+          <dd class="mt-2 text-base leading-7 text-gray-600">25 Februari 2023.</dd>
+        </div>
+
+        <div class="relative pl-16">
+          <dt class="text-base font-semibold leading-7 text-gray-900">
+            <div class="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+         
+            </div>
+            Alamat :
+          </dt>
+          <dd class="mt-2 text-base leading-7 text-gray-600"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.069190566711!2d106.7798123147693!3d-6.254614995472744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1e0ca077a7b%3A0xb79e7c16af6422b5!2sKeisha%20Glow%20Jakarta!5e0!3m2!1sen!2sid!4v1676886868162!5m2!1sen!2sid" width="200" height="200"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>.</dd>
+        </div>
+        
+      </dl>
+    </div>
+  </div>
+</div>
+
+
+<section class="overflow-hidden text-gray-700">
+  <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+    <div class="flex flex-wrap -m-1 md:-m-2">
+      <div class="flex flex-wrap w-1/2">
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="https://pict.sindonews.net/dyn/620/makassar/news/2019/08/27/1/30580/klinik-kecantikan-keisha-raih-penghargaan-dari-menteri--qyw.jpg"/>
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="https://www.kureta.id/Asset/uploads/1619275707506-klinik-kecantikan.jpg"/>
+        </div>
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="https://keishaglow.com/wp-content/uploads/2021/03/logo-keisha-gold-gradasi-gelap.png"/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/2">
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="https://keishaglow.com/wp-content/uploads/2021/04/treatment-headline.jpg"/>
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="https://i0.wp.com/inipasti.com/wp-content/uploads/2021/01/IMG_20210116_205150.jpg?fit=1200%2C618&ssl=1&w=640"/>
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="https://pluz.id/assets/media/upload/2021/01/Peresmian-Keisha-Glow.jpg"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<div class="overflow-hidden bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+      <div class="lg:pr-8 lg:pt-4">
+        <div class="lg:max-w-lg">
+          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Rundown</p>
+          <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+            <div class="relative pl-9">
+              <dt class="inline font-semibold text-gray-900">
+                <svg class="absolute top-1 left-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
+                </svg>
+                10.00 - 10.05.
+              </dt>
+              <dd class="inline">MC Opening.</dd>
+            </div>
+
+            <div class="relative pl-9">
+              <dt class="inline font-semibold text-gray-900">
+                
+                10.05 - 10.20.
+              </dt>
+              <dd class="inline">Sambutan Owner.</dd>
+            </div>
+
+            <div class="relative pl-9">
+              <dt class="inline font-semibold text-gray-900">
+                
+                10.20 - 10.40.
+              </dt>
+              <dd class="inline">Opening Ceremony.</dd>
+            </div>
+
+            <div class="relative pl-9">
+              <dt class="inline font-semibold text-gray-900">
+                
+                10.40 - 11.00.
+              </dt>
+              <dd class="inline">Pengenalan Klinik & Product.</dd>
+            </div>
+
+            <div class="relative pl-9">
+              <dt class="inline font-semibold text-gray-900">
+                
+                11.00 - 11.20.
+              </dt>
+              <dd class="inline">Penandatanganan Kerjasama Mitra.</dd>
+            </div>
+
+            <div class="relative pl-9">
+              <dt class="inline font-semibold text-gray-900">
+                
+                11.20 - 12.00.
+              </dt>
+              <dd class="inline">Launch & Closing.</dd>
+            </div>
+
+            <div class="relative pl-9">
+              <dt class="inline font-semibold text-gray-900">
+                
+                12.00 - Selesai.
+              </dt>
+              <dd class="inline">Tour Clinic.</dd>
+            </div>
+            </dl></div></div>
+
+      <img src="https://keishaglow.com/wp-content/uploads/2021/04/treatment-headline.jpg" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442"/>
+    </div>
+  </div>
+</div>
+
+            </div>
+        </div>
+    )
+}
